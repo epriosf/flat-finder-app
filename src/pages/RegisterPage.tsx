08aa1b3458a1836d2e7ed37563dc6a08c6aecd87
Erick Rios <paulrios@outlook.es>
2024-08-19 19:52:31 -0500

    feat: add validation formik to register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,12 +1,16 @@
+import { useFormik } from 'formik';
 import { Button } from 'primereact/button';
 import { Calendar } from 'primereact/calendar';
-import { FileUpload } from 'primereact/fileupload';
+import { FileUpload, FileUploadHandlerEvent } from 'primereact/fileupload';
 import { FloatLabel } from 'primereact/floatlabel';
+import { Message } from 'primereact/message';
 import { Password } from 'primereact/password';
 import { Nullable } from 'primereact/ts-helpers';
 import { useState } from 'react';
 import { styled } from 'styled-components';
-import FloatingInput, { FormField } from '../components/Commons/FloatingInput';
+import * as Yup from 'yup';
+import FloatingInput from '../components/Commons/FloatingInput';
+import { createUser, uploadProfileImage } from '../services/firebase';
 
 const StyledFormContainer = styled.div`
   display: flex;
@@ -21,49 +25,136 @@ const StyledFormContainer = styled.div`
   backdrop-filter: blur(10px);
   width: 100%;
 `;
+//Min and Max Dates
+const today = new Date();
+let minDate = new Date(today);
+let maxDate = new Date(today);
+
+//User Type
+export interface User {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  birthday: Date;
+  profile: string;
+}
+
+minDate.setFullYear(today.getFullYear() - 120);
+maxDate.setFullYear(today.getFullYear() - 18);
+
+const SignupSchema = Yup.object({
+  firstName: Yup.string().required('First Name Required'),
+  lastName: Yup.string().required('Last Name Required'),
+  email: Yup.string().email('Invalid email').required('Email Required'),
+  birthday: Yup.date()
+    .required('Birth Date Required')
+    .min(minDate, 'Date can not be 120 years more ago')
+    .max(maxDate, 'Date can not be more than 18 years ago'),
+  password: Yup.string().required('Password Required'),
+  passwordConfirm: Yup.string()
+    .oneOf([Yup.ref('password')], 'Passwords must match')
+    .required('Password Confirm Required'),
+});
 
 const RegisterPage = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState<Nullable<Date>>(null);
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [profileUrl, setProfileUrl] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      birthday: null,
+      password: '',
+      passwordConfirm: '',
+    },
+    validationSchema: SignupSchema,
+    onSubmit: async (values) => {
+      const user: User = {
+        firstname: values.firstName,
+        lastname: values.lastName,
+        email: values.email,
+        birthday: values.birthday ? new Date(values.birthday) : new Date(today),
+        password: values.password,
+        profile: profileUrl ? profileUrl : '',
+      };
+      await createUser(user);
+    },
+  });
 
-  const handleRegister = () => {
-    console.log('handle Register');
+  const handleBirthdayChange = (e: Nullable<Date>) => {
+    formik.setFieldValue('birthday', e);
+    formik.setFieldTouched('birthday', true); // Mark birthday field as touched to trigger validation
   };
+
+  const handleUploadImage = async (e: FileUploadHandlerEvent) => {
+    const file = e.files[0];
+    const imageUrl = await uploadProfileImage(file);
+    setProfileUrl(imageUrl);
+    console.log('Image Url', imageUrl);
+  };
+
   return (
     <>
       <StyledFormContainer>
-        <FloatingInput
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          iconClass="pi pi-user"
-          label="First Name"
-          type="text"
-        />
+        <form onSubmit={formik.handleSubmit} className="w-full">
+          <FloatingInput
+            id="firstName"
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+            iconClass="pi pi-user"
+            label="First Name"
+            type="text"
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <Message
+              severity="error"
+              text={formik.errors.firstName}
+              className="mb-1 pt-1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FloatingInput
-          id="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          iconClass="pi pi-user"
-          label="Last Name"
-          type="text"
-        ></FloatingInput>
+          <FloatingInput
+            id="lastName"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+            iconClass="pi pi-user"
+            label="Last Name"
+            type="text"
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <Message
+              severity="error"
+              text={formik.errors.lastName}
+              className="mb-1 mt--1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FloatingInput
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          iconClass="pi pi-envelope"
-          label="Email"
-          type="text"
-        ></FloatingInput>
+          <FloatingInput
+            id="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            iconClass="pi pi-envelope"
+            label="Email"
+            type="text"
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Message
+              severity="error"
+              text={formik.errors.email}
+              className="mb-1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FormField>
           <FloatLabel>
             <div className="p-inputgroup">
               <span className="p-inputgroup-addon">
@@ -71,19 +162,28 @@ const RegisterPage = () => {
               </span>
               <Calendar
                 inputId="birth_date"
-                value={birthday}
-                onChange={(e) => setBirthday(e.value)}
-                className="w-full"
+                value={formik.values.birthday}
+                onChange={(e) => handleBirthdayChange(e.value)}
                 inputStyle={{ borderLeft: 'none' }}
+                minDate={minDate}
+                dateFormat="dd/mm/yy"
               />
             </div>
             <label htmlFor="birth_date" className="floating-label pl-5">
               Birth Date
             </label>
           </FloatLabel>
-        </FormField>
+          {formik.touched.birthday && formik.errors.birthday ? (
+            <Message
+              severity="error"
+              text={formik.errors.birthday}
+              className="mb-1 mt--1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FormField>
           <FloatLabel>
             <div className="p-inputgroup ">
               <span className="p-inputgroup-addon">
@@ -91,34 +191,41 @@ const RegisterPage = () => {
               </span>
               <Password
                 inputId="password"
-                value={password}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setPassword(e.target.value)
-                }
+                value={formik.values.password}
+                onChange={formik.handleChange}
                 className="w-full"
                 inputStyle={{ borderLeft: 'none' }}
+                onBlur={formik.handleBlur}
+                toggleMask
               />
             </div>
             <label htmlFor="password" className="floating-label pl-5 w-full">
               Password
             </label>
           </FloatLabel>
-        </FormField>
+          {formik.touched.password && formik.errors.password ? (
+            <Message
+              severity="error"
+              text={formik.errors.password}
+              className="mb-1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FormField>
           <FloatLabel>
             <div className="p-inputgroup ">
               <span className="p-inputgroup-addon">
                 <i className="pi pi-lock"></i>
               </span>
               <Password
-                inputId="password"
-                value={passwordConfirm}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setPasswordConfirm(e.target.value)
-                }
+                inputId="passwordConfirm"
+                value={formik.values.passwordConfirm}
+                onChange={formik.handleChange}
                 className="w-full"
                 inputStyle={{ borderLeft: 'none' }}
+                onBlur={formik.handleBlur}
               />
             </div>
             <label
@@ -128,12 +235,19 @@ const RegisterPage = () => {
               Confirm Password
             </label>
           </FloatLabel>
-        </FormField>
+          {formik.touched.passwordConfirm && formik.errors.passwordConfirm ? (
+            <Message
+              severity="error"
+              text={formik.errors.passwordConfirm}
+              className="mb-1"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          ) : (
+            <div className="mt-5"></div>
+          )}
 
-        <FormField>
           <FileUpload
             name="demo[]"
-            url={'/api/upload'}
             multiple
             accept="image/*"
             maxFileSize={1000000}
@@ -141,15 +255,12 @@ const RegisterPage = () => {
               <p className="m-0">Drag and drop images to here to upload.</p>
             }
             chooseLabel="Photo"
+            customUpload
+            uploadHandler={handleUploadImage}
           />
-        </FormField>
 
-        <Button
-          label="Sign up"
-          className="w-full"
-          onClick={handleRegister}
-          type="submit"
-        />
+          <Button label="Sign up" className="w-full mt-5" type="submit" />
+        </form>
       </StyledFormContainer>
     </>
   );
